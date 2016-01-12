90dc28042f650f3ecf2da84062cf96904c54f6b4
Rick Aites <rick8s@hotmail.com>
2016-01-12 00:10:01 -0600

    Added new image and link to project. Edited profile.

--- javascripts/controllers/portfolio.js
+++ javascripts/controllers/portfolio.js
@@ -1,4 +1,6 @@
 app.controller("PortfolioCtrl", ["$scope", "$http", "$location", "$sce", 
   function($scope, $http, $location, $sce) {
-   $scope.calImgSrc = $sce.trustAsResourceUrl('img/calendar.png');    
+   $scope.calImgSrc = $sce.trustAsResourceUrl('img/calendar.png'); 
+   $scope.toolImgSrc = $sce.trustAsResourceUrl('img/toolshed.png'); 
+
 }]); //closes lines 1-2
